c05cf5ab0688cacb0f470dfb6760df2c1e22ff89
C Anthony Risinger <c@anthonyrisinger.com>
2014-07-08 05:34:20 -0500

    [PRD-2] impl dump(s); prefer _json

--- zippy/json.py
+++ zippy/json.py
@@ -1,21 +1,33 @@
-def loads(s, *args, **kwds):
-    """json.loads substitute
-    """
-    # 0x20000: unicode_literals
-    # 0x02000: division
-    co = compile(s, '<json>', 'eval', 0x22000, True)
-    ns = {'true': True, 'false': False, 'null': None}
-    js = eval(co, ns)
-    return js
+try:
+    from json import dump
+    from json import load
+    from json import dumps
+    from json import loads
+except ImportError:
+    def dump(o, fp, *args, **kwds):
+        """json.dump substitute
+        """
+        return fp.write(dumps(o, *args, **kwds))
 
+    def load(fp, *args, **kwds):
+        """json.load substitute
+        """
+        return loads(fp.read(), *args, **kwds)
 
-def dumps(*args, **kwds):
-    pass
+    def dumps(o, *args, **kwds):
+        """json.dumps substitute
+        """
+        return pf(o)
 
-
-def load(fp, *args, **kwds):
-    return loads(fp.read(), *args, **kwds)
-
-
-def dump(*args, **kwds):
-    pass
+    def loads(o, *args, **kwds):
+        """json.loads substitute
+        """
+        try:
+            # 0x20000: unicode_literals
+            # 0x02000: division
+            co = compile(o, '<json>', 'eval', 0x22000, True)
+            ns = {'true': True, 'false': False, 'null': None}
+            js = eval(co, ns)
+            return js
+        except (SyntaxError, NameError) as e:
+            raise ValueError(*e)
